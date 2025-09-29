2c948734c1fa3838990179d5f26328b40ebb1d12
Henry Carr <henry.carr98@gmail.com>
2025-09-29 15:23:11 +0100

    latest test data

---
+++ var_thread/plotter.py
@@ -0,0 +1,26 @@
+import sys
+import matplotlib.pyplot as plt
+import csv
+
+xs = []
+ys = []
+
+with open(sys.argv[1]) as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    for row in reader:
+        threads = int(row[0])
+        time_sec = float(row[1])
+        xs.append(threads)
+        ys.append(time_sec)
+
+plt.figure(figsize=(6,4))
+plt.plot(xs, ys, marker="o", linestyle="-", color="b")
+
+plt.xlabel("Threads")
+plt.ylabel("Execution Time (s)")
+plt.title("Execution Time vs Threads")
+plt.grid(True)
+
+# Save plot
+plt.savefig("../img/plot_threads.pdf")
